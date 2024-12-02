6bfedeeb11f77a5c1cc6dcd720b8e1f51e3300cc
Steve Turoscy <sturoscy@globus.org>
2024-12-02 12:56:09 -0500

    Updating settings files

--- src/settings/local.py
+++ src/settings/local.py
@@ -21,26 +21,6 @@ policy_path = "file://" + policy_path
 # Load the .env file
 load_dotenv(env_path)
 
-# ESGF2 Globus Project
-project_id = "cae45630-2a4b-47b9-b704-d870e341da67"
-
-# ESGF2 STAC Transaction API client
-publisher = {
-    "client_id": "ec5f07c0-7ed8-4f2b-94f2-ddb6f8fc91a3",
-    "redirect_uri": "https://auth.globus.org/v2/web/auth-code",
-}
-
-# ESGF2 STAC Transaction API service
-stac_api = {
-    "client_id": os.environ.get("STAC_LIENT_ID"),
-    "client_secret": os.environ.get("STAC_CLIENT_SECRET"),
-    "issuer": "https://auth.globus.org",
-    "access_control_policy": policy_path,
-    "scope_id": "ca49f459-a4f8-420c-b55f-194df11abc0f",
-    "scope_string": "https://auth.globus.org/scopes/6fa3b827-5484-42b9-84db-f00c7a183a6a/ingest",
-    "url": "0.0.0.0:9000",
-}
-
 event_stream = {
     "config": {
         "bootstrap.servers": "host.docker.internal:9092",
@@ -53,14 +33,11 @@ event_stream = {
 }
 
 globus_search_client_credentials = {
-    "client_id": os.environ.get("SEARCH_CLIENT_ID"),
-    "client_secret": os.environ.get("SEARCH_CLIENT_SECRET")
+    "client_id": os.environ.get("CLIENT_ID"),
+    "client_secret": os.environ.get("CLIENT_SECRET")
 }
-
-print(globus_search_client_credentials)
 
 # ESGF2 Globus Search
 globus_search = {
     "index": "58f3a9eb-51f8-410e-8e68-da4bd6617ca6",
-    # "index": "f037bb33-3413-448b-8486-8400bee5181a",
 }
--- src/settings/production.py
+++ src/settings/production.py
@@ -1,27 +1,10 @@
 import os
+from dotenv import load_dotenv
 from utils import get_secret
 
 
-# ESGF2 Globus Project
-project_id = "cae45630-2a4b-47b9-b704-d870e341da67"
-
-# ESGF2 STAC Transaction API client
-publisher = {
-    "client_id": "ec5f07c0-7ed8-4f2b-94f2-ddb6f8fc91a3",
-    "redirect_uri": "https://auth.globus.org/v2/web/auth-code",
-}
-
-# ESGF2 STAC Transaction API service
-stac_api = {
-    "client_id": "6fa3b827-5484-42b9-84db-f00c7a183a6a",
-    "client_secret": os.environ.get("CLIENT_SECRET"),
-    "issuer": "https://auth.globus.org",
-    "access_control_policy": "https://esgf2.s3.amazonaws.com/access_control_policy.json",
-    "admins": "https://esgf2.s3.amazonaws.com/admins.json",
-    "scope_id": "ca49f459-a4f8-420c-b55f-194df11abc0f",
-    "scope_string": "https://auth.globus.org/scopes/6fa3b827-5484-42b9-84db-f00c7a183a6a/ingest",
-    "url": "https://n08bs7a0hc.execute-api.us-east-1.amazonaws.com/dev",
-}
+# Load the .env file
+load_dotenv()
 
 # ESGF2 Event Stream Service Producer/Consumer
 kafka_secretsmanager = {
@@ -46,7 +29,7 @@ event_stream = {
     "topics": ["esgf2"],
 }
 
-if os.environ.get("KAFKA_CLIENT_DEBUG"):
+if os.environ.get("KAFKA_CLIENT_DEBUG", False):
     event_stream["config"]["debug"] = "all"
     event_stream["config"]["log_level"] = 7
 
